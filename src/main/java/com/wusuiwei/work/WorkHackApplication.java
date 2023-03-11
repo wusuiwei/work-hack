6b2571d3a5c7bfffa4c93a6637bc3966f2b1637e
wsw <1763721508@qq.com>
2023-03-11 20:30:07 +0800

    初始化work-hack项目

---
+++ src/main/java/com/wusuiwei/work/WorkHackApplication.java
@@ -0,0 +1,13 @@
+package com.wusuiwei.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHackApplication.class, args);
+	}
+
+}
