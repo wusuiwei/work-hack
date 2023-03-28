d626fc335b0d7750b9666ec6051feef9a6ca97fc
wsw <1763721508@qq.com>
2023-03-28 17:36:47 +0800

    mybatis-generator

---
+++ mybatis-generator/src/main/java/com/wusuiwei/generator/MybatisGenerator.java
@@ -0,0 +1,26 @@
+package com.wusuiwei.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGenerator {
+    public static void main(String[] args) {
+        try {
+            List<String> warnings = new ArrayList<>();
+            File configFile = new File("mybatis-generator/src/main/resources/generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
